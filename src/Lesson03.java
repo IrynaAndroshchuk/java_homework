d8a360ab0c15e3d2a41f739f4955c2f70c54b982
Andriy Hrechyn <andriy.hrechyn@gmail.com>
2019-10-16 21:51:31 +0300

    Homework Lesson 3

---
+++ src/Lesson03.java
@@ -0,0 +1,30 @@
+public class Lesson03 {
+    public static void main(String[] args) {
+        String day = "Sunday";
+        switch (day) {
+            case "Monday":
+                System.out.println("Monday plans");
+                break;
+            case "Tuesday":
+                System.out.println("Tuesday plans");
+                break;
+            case "Wednesday":
+                System.out.println("Wednesday plans");
+                break;
+            case "Thursday":
+                System.out.println("Thursday plans");
+                break;
+            case "Friday":
+                System.out.println("Friday plans");
+                break;
+            case "Saturday":
+                System.out.println("Saturday plans");
+                break;
+            case "Sunday":
+                System.out.println("Sunday plans");
+                break;
+            default:
+                System.out.println("Friday 13");
+        }
+    }
+}
