584d24bac9ce12ac4e0ea4530a8ba7f2cd7a0b89
Andriy Hrechyn <andriy.hrechyn@gmail.com>
2019-10-16 22:20:43 +0300

    Homework Lesson 4 Task 1-2

---
+++ src/Lesson04Task1.java
@@ -0,0 +1,26 @@
+public class Lesson04Task1 {
+    public static void main(String[] args) {
+        int a = 11;
+//        float b = 1.34334;
+        double c = 3.4;
+        char d = '1';
+        boolean e = true;
+        byte f = 1;
+        long g = 434585544;
+        short j = 3;
+
+        System.out.println("Integer Max Value: " + Integer.MAX_VALUE);
+        System.out.println("Integer Min Value: " + Integer.MIN_VALUE);
+        System.out.println("Float Max Value: " + Float.MAX_VALUE);
+        System.out.println("Float Min Value: " + Float.MIN_VALUE);
+        System.out.println("Double Max Value: " + Double.MAX_VALUE);
+        System.out.println("Double Min Value: " + Double.MIN_VALUE);
+        System.out.println("Byte Max Value: " + Byte.MAX_VALUE);
+        System.out.println("Byte Min Value: " + Byte.MIN_VALUE);
+        System.out.println("Short Max Value: " + Short.MAX_VALUE);
+        System.out.println("Short Min Value: " + Short.MIN_VALUE);
+        System.out.println("Long Max Value: " + Long.MAX_VALUE);
+        System.out.println("Long Min Value: " + Long.MIN_VALUE);
+
+    }
+}
