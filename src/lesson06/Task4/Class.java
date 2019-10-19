b215b673eed22473f8c2ecf61cd9e5d4596dd2a0
Iryna Androshchuk <Airenka95@gmail.com>
2019-10-19 21:16:43 +0300

    Lesson 6 Task 4

---
+++ src/lesson06/Task4/Class.java
@@ -0,0 +1,18 @@
+package lesson06.Task4;
+
+public class Class {
+    private int a;
+    private int b;
+    private int c;
+
+    public Class(int a, int b) {
+        this(4);
+    }
+
+    public Class(int c) {
+        this.c = c;
+    }
+}
+
+
+
