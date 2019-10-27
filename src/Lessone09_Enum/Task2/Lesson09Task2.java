612a81a5eb0c6b36bfeaf280df97db0138b533fe
Iryna Androshchuk <airenka95@gmail.com>
2019-10-27 12:33:28 +0200

    lesson09 Task2 Scanner

--- src/Lessone09_Enum/Task2/Lesson09Task2.java
+++ src/Lessone09_Enum/Task2/Lesson09Task2.java
@@ -8,9 +8,14 @@ public class Lesson09Task2 {
         System.out.print("Enter number: ");
 
         Scanner scan = new Scanner(System.in);
-        int number = scan.nextInt();
+        double number = Double.parseDouble(scan.nextLine());
 
-        if (number / 2 == 0) {
+        if (number != (int) number) {
+            System.out.println("Your number is not integer");
+            System.exit(0);
+        }
+
+        if (number % 2 == 0) {
             System.out.println("Your number is even");
         } else {
             System.out.println("Your number is odd");
