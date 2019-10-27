69b31031774473ac37b3a08d2d45f469c9cba939
Iryna Androshchuk <airenka95@gmail.com>
2019-10-27 12:49:47 +0200

    lesson09 Task3 Scanner

---
+++ src/Lessone09_Enum/Task3/Lesson09Task3.java
@@ -0,0 +1,17 @@
+package Lessone09_Enum.Task3;
+
+import java.util.Scanner;
+
+public class Lesson09Task3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter number 1 : ");
+        int number1 = scan.nextInt();
+
+        System.out.print("Enter number 2 : ");
+        int number2 = scan.nextInt();
+
+        System.out.println("Number1 + number 2 = " + (number1 + number2));
+    }
+}
